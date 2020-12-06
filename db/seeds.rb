5a2ca6b1ab4d28f028203d27af83b1b0f3236606
Ahmed <ah01567@surrey.ac.uk>
2020-12-06 23:30:54 +0000

    made seeds for my DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+surrey = Uni.where(name: 'Surrey').first
+surrey.delete if surrey
+unis = Uni.create([name: 'Surrey', location: 'Guildford'])
