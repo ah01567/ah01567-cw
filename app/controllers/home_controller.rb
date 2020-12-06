03c2b958b7075dc05d6076620ee2fc297ed225ca
Ahmed <ah01567@surrey.ac.uk>
2020-12-06 22:30:36 +0000

    added contact page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def home
   end
+
+  def contact
+  end
+
+  def ask_for_contact
+    name = params[:name]
+    telephone = params[:telephone]
+    email = params[:email]
+    country = params[:country]
+    university_name = params[:university_name]
+    message = params[:message]
+    if email.blank?
+      flash[:alert] = I18n.t('home.ask_for_contact.add_email')
+    else
+      
+      flash[:notice] = I18n.t('home.ask_for_contact.email_sent')
+    end
+
+   redirect_to root_path
+  end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test "bring home page" do
     get root_url
     assert_response :success
   end
 
+  test "bring contact page" do
+    get contact_url
+    assert_response :success
+    assert_template layout: 'application'
+    assert_select 'title', 'Ah01567Com2025Project'
+    assert_select 'h1', 'Ask our team !'
+    assert_select 'p', 'If you have any enquiries about any university or department field, please feel free to ask .'
+  end
+
+  test "post ask for contact" do
+    post ask_for_contact_url, params:
+      {name: "Ahmed", email: "Ahmed@gmail.com", telephone: "0123456789", country: "UK", university_name: "Surrey", ssage: "Hi its me"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
 end
