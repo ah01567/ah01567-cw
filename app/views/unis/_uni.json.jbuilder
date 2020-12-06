a712789be565d9dbe26270d121bb42c85a7d042d
Ahmed <ah01567@surrey.ac.uk>
2020-12-06 23:18:52 +0000

    added scaffold Uni

---
+++ app/views/unis/_uni.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! uni, :id, :name, :location, :created_at, :updated_at
+json.url uni_url(uni, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :unis
   root 'home#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'contact', to: 'home#contact'
